869f41fade6910eeb03390389ee111a6fe54bcf1
DaniiMello <daniellemello.p@gmail.com>
2022-08-31 15:01:52 -0300

    exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,4 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+minhaString2=minhaString.replace("________","sticioso")
+console.log (minhaString)
+console.log ("Antes da remoção dos espaços, tinham "+minhaString.length+" caracteres. \nDepois da remoção dos espaços, ficaram " +minhaString.trim().length+" caracteres.\n" +minhaString2)
